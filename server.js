88d09efd50592b61c3b49555de469f4bf72a2095
aradbivas <bivasarad@gmail.com>
2023-01-22 15:38:43 +0200

    Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const cors = require('cors')
+const serverless = require('serverless-http');
 const app = express();
 const reportRoutes = require('./Routes/Report')
 const userRoutes = require('./Routes/User')
@@ -11,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({
     type: ['application/json', 'text/plain']
 }))
-app.use('/api/report', reportRoutes)
-app.use('/api/user', userRoutes)
+app.use('/.netlify/functions/api/report', reportRoutes)
+app.use('/.netlify/functions/api/user', userRoutes)
 
 mongoose.connect("mongodb+srv://" + process.env.password +"@cluster0.rfa43.mongodb.net/Costs");
 
@@ -20,3 +21,5 @@ mongoose.connect("mongodb+srv://" + process.env.password +"@cluster0.rfa43.mongo
 app.listen(4020, function() {
     console.log("Server started on port 4020");
 });
+
+module.exports.handler = serverless(app);
